d0c927c0e2c67f5eee4c4073a5f247dba836c61f
EP\hunng <manhhung.nguyen@optimizely.com>
2022-10-10 10:06:04 +0700

    Action create jira release

---
+++ release/index.js
@@ -0,0 +1,29 @@
+const core = require('@actions/core');
+const { checkEnv, run } = require('./utils');
+const { createJiraRelease } = require('./jira-release');
+
+const action = async () => {
+  checkEnv(['JIRA_USERNAME', 'JIRA_PASSWORD']);
+
+  const protocol = core.getInput('jira-protocol', { required: true });
+  const host = core.getInput('jira-host', { required: true });
+  const projectKey = core.getInput('jira-project', { required: true });
+  const component = core.getInput('jira-component') || '';
+  const package  = core.getInput('jira-package') || '';
+  const version = core.getInput('version', { required: true });
+
+  await createJiraRelease({
+    protocol,
+    host,
+    projectKey,
+    component,
+    version,
+    package
+  });
+};
+
+if (require.main === module) {
+  run(action);
+}
+
+module.exports = action;
---
+++ release/utils.js
@@ -0,0 +1,25 @@
+const core = require('@actions/core');
+
+/**
+ * Run an async action and catch any exception.
+ * @param action the action to run
+ * @returns {Promise<void>}
+ */
+const run = async (action) => {
+  try {
+    await action();
+  } catch (err) {
+    core.setFailed(err.message);
+  }
+};
+
+const checkEnv = (variables) => {
+    variables.every((name) => {
+      if (!process.env[name]) {
+        throw new Error(`Missing env var: ${name}`);
+      }
+      return true;
+    });
+  };
+  
+  module.exports = {checkEnv,run};
